667e61b668d06813fe3603c417c55769e811ec8c
AlexCsanyi <alex.csanyi@gmail.com>
2019-08-05 07:42:57 +0100

    set up all basic requirements and deleted unneccessary code

--- src/index.js
+++ src/index.js
@@ -1,10 +1,25 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import * as serviceWorker from "./serviceWorker";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const firebase = require("firebase");
+require("firebase/firestore");
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAIU1KqCypli-XXVPgElaGtXM6yxTVzT0M",
+  authDomain: "note-360.firebaseapp.com",
+  databaseURL: "https://note-360.firebaseio.com",
+  projectId: "note-360",
+  storageBucket: "note-360.appspot.com",
+  messagingSenderId: "948233902939",
+  appId: "1:948233902939:web:e3e91cc39d8b336c"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+ReactDOM.render(<App />, document.getElementById("note-360"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
