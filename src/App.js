c7d26ab6f58c8e787bbe68401703cef1af9d3f33
AlexCsanyi <alex.csanyi@gmail.com>
2019-08-06 07:46:20 +0100

    started to work on the app component

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React from "react";
+import SidebarComponent from "./sidebar/sidebar";
+import EditorComponent from "./editor/editor";
 import "./App.css";
 
 const firebase = require("firebase");
@@ -14,7 +16,12 @@ class App extends React.Component {
   }
 
   render() {
-    return <div>Hello World</div>;
+    return (
+      <div className="app-container">
+        <SidebarComponent />
+        <EditorComponent />
+      </div>
+    );
   }
 
   componentDidMount = () => {
