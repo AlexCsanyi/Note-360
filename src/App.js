22636cb536be50da843df17bebdbb6c045730ecb
AlexCsanyi <alex.csanyi@gmail.com>
2019-08-06 06:33:35 +0100

    set up basic state and how notes are accessed from firebase

--- src/App.js
+++ src/App.js
@@ -1,10 +1,36 @@
 import React from "react";
 import "./App.css";
 
+const firebase = require("firebase");
+
 class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      selectedNoteIndex: null,
+      selectedNote: null,
+      notes: null
+    };
+  }
+
   render() {
     return <div>Hello World</div>;
   }
+
+  componentDidMount = () => {
+    firebase
+      .firestore()
+      .collection("notes")
+      .onSnapshot(serverUpdate => {
+        const notes = serverUpdate.docs.map(_doc => {
+          const data = _doc.data();
+          data["id"] = _doc.id;
+          return data;
+        });
+        console.log(notes);
+        this.setState({ notes: notes });
+      });
+  };
 }
 
 export default App;
