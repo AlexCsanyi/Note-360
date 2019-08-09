c644643d9bc673e978ef1aada3cca808d9d6fe9a
AlexCsanyi <alex.csanyi@gmail.com>
2019-08-09 08:07:40 +0100

    delete function added

--- src/App.js
+++ src/App.js
@@ -67,6 +67,53 @@ class App extends React.Component {
         timestamp: firebase.firestore.FieldValue.serverTimestamp()
       });
   };
+
+  newNote = async title => {
+    const note = {
+      title: title,
+      body: ""
+    };
+    const newFromDB = await firebase
+      .firestore()
+      .collection("notes")
+      .add({
+        title: note.title,
+        body: note.body,
+        timestamp: firebase.firestore.FieldValue.serverTimestamp()
+      });
+    const newID = newFromDB.id;
+    await this.setState({ notes: [...this.state.notes, note] });
+    const newNoteIndex = this.state.notes.indexOf(
+      this.state.notes.filter(_note => _note.id === newID)[0]
+    );
+    this.setState({
+      selectedNote: this.state.notes[newNoteIndex],
+      selectedNoteIndex: newNoteIndex
+    });
+  };
+
+  deleteNote = async note => {
+    const noteIndex = this.state.notes.indexOf(note);
+    await this.setState({
+      notes: this.state.notes.filter(_note => _note !== note)
+    });
+    if (this.state.selectedNoteIndex === noteIndex) {
+      this.setState({ selectedNoteIndex: null, selectedNote: null });
+    } else {
+      this.state.notes.length > 1
+        ? this.selectNote(
+            this.state.notes[this.state.selectedNoteIndex - 1],
+            this.state.selectedNoteIndex - 1
+          )
+        : this.setState({ selectedNoteIndex: null, selectedNote: null });
+    }
+
+    firebase
+      .firestore()
+      .collection("notes")
+      .doc(note.id)
+      .delete();
+  };
 }
 
 export default App;
--- src/sidebar/sidebar.js
+++ src/sidebar/sidebar.js
@@ -71,15 +71,19 @@ class SidebarComponent extends React.Component {
   };
 
   newNote = () => {
-    console.log(this.state);
+    this.props.newNote(this.state.title);
+    this.setState({
+      title: null,
+      addingNote: false
+    });
   };
 
   selectNote = (n, i) => {
     this.props.selectNote(n, i);
   };
 
-  deleteNote = () => {
-    console.log("delete note");
+  deleteNote = note => {
+    this.props.deleteNote(note);
   };
 }
 
