0fcdde7173389dc50a6bb29416b55e49efefb761
AlexCsanyi <alex.csanyi@gmail.com>
2019-08-09 06:58:37 +0100

    added lifecyle hooks to the editor component

--- src/editor/editor.js
+++ src/editor/editor.js
@@ -16,6 +16,24 @@ class EditorComponent extends React.Component {
     };
   }
 
+  componentDidMount = () => {
+    this.setState({
+      text: this.props.selectedNote.body,
+      title: this.props.selectedNote.title,
+      id: this.props.selectedNote.id
+    });
+  };
+
+  componentDidUpdate = () => {
+    if (this.props.selectedNote.id !== this.state.id) {
+      this.setState({
+        text: this.props.selectedNote.body,
+        title: this.props.selectedNote.title,
+        id: this.props.selectedNote.id
+      });
+    }
+  };
+
   render() {
     const { classes } = this.props;
 
