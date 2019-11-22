089039830817ea4200fcb31f6e2cc54bfa5f1e9b
AlexCsanyi <alex.csanyi@gmail.com>
2019-11-22 06:01:35 +0000

    deleted folder

---
+++ src/landingpage/landing.js
@@ -0,0 +1,11 @@
+import React from "react";
+import { withStyles } from "@material-ui/core/styles";
+import styles from "./styles";
+
+class LandingComponent extends React.Component {
+  render() {
+    return <div></div>;
+  }
+}
+
+export default withStyles(styles)(LandingComponent);
