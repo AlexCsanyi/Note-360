93724199fb5b9ca77c4d2c9cbc62193d58607da9
AlexCsanyi <alex.csanyi@gmail.com>
2019-11-25 07:23:44 +0000

    started adding more content

--- src/landingpage/landing.js
+++ src/landingpage/landing.js
@@ -14,7 +14,30 @@ class LandingComponent extends React.Component {
           <Icons></Icons>
           <Footer></Footer>
         </div>
-        <div className="secondary"></div>
+        <div className="secondary">
+          <div id="secondary-text-container">
+            <h2>
+              Introducing <span id="green-subtitle">NOTE 360</span>, an App
+              Designed for <span id="green-underline">Better Performance</span>.
+            </h2>
+            <p id="secondary-bold-paragraph">
+              Designed to help organize notes and make it easier to review your
+              content.
+            </p>
+            <div className="secondary-info">
+              <p className="secondary-info-title">Users</p>
+              <p className="secondary-info-title">Likes</p>
+              <p className="secondary-info-value">1,500</p>
+              <p className="secondary-info-value">4,200</p>
+            </div>
+            <div className="secondary-noMargin">
+              <p className="secondary-info-title">W.R.I.T.E.</p>
+              <p className="secondary-info-value">
+                What Topic | Review | Investigate | Time Yourself | Exit Smart
+              </p>
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
